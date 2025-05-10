1504b58d451ef22cd495a1ed8b434f09955b5fd0
ihbrazil <ihbrazil@users.noreply.github.com>
2025-05-10 20:56:48 -0300

    atualização meta tags e ícones

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,6 +10,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Projeto Prevenus" />
+        <meta name="keywords" content="Prevenus, alongamentos, saúde, ergonomia" />
+        <meta name="author" content="ihbrazil" />
+        <meta name="robots" content="index, follow" />
+        <meta name="google-site-verification" content="your-google-site-verification-code" />
+        <meta property="og:title" content="Projeto Prevenus" />
+        <meta property="og:description" content="Projeto Prevenus - Prevenção de Doenças Ocupacionais em Informática" />
+      </head>
       <body>
         <Topo/>
         {children}
