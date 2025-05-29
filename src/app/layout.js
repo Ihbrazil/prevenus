8d9fe4fccede442856606c647427962b5d10e1e0
ihbrazil <ihbrazil@users.noreply.github.com>
2025-05-29 16:20:45 -0300

    Atualização dos ícones
    Movimentação dos ícones para pasta public do Prevenus e alteração link favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <head>       
-        <link rel="icon" href="./public/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Projeto Prevenus" />
         <meta name="keywords" content="Prevenus, alongamentos, saúde, ergonomia" />
