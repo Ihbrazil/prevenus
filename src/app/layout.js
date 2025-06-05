c2ce5c4249523f1748815751e89d66f5d4801f76
ihbrazil <ihbrazil@users.noreply.github.com>
2025-06-05 07:34:49 -0300

    Atualização GoogleTagManager

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 import Topo from "../componentes/Topo";
 import Rodape from "../componentes/Rodape";
-import Script from "next/script";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 export const metadata = {
   title: "Projeto Prevenus",
@@ -13,25 +13,8 @@ export default function RootLayout({ children }) {
     <html lang="pt-br">
       <head>
         {/* Google Tag Manager Script */}
-        <Script 
-          id="google-tag-manager" 
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s), dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','G-C954W9XDEG');
-            `,
-          }}
-        />
+        <GoogleTagManager gtmId="G-C954W9XDEG" />
         {/* Fim Google Tag Manager */}
-
         {/* Meta Tags */}
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -44,17 +27,6 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://prevenus.com.br" />
       </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=G-C954W9XDEG"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-        {/* Fim Google Tag Manager (noscript) */}
-
         {/* Estrutura principal */}
         <Topo />
         {children}
